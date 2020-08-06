ba865e9613d9453e73f071ef05a316c3fe323463
LeanNSP <nsp.lean@gmail.com>
2020-08-06 09:20:33 +0300

    edit createCardsFunc

--- src/utils/createCardsFunc.js
+++ src/utils/createCardsFunc.js
@@ -1,5 +1,5 @@
 import itemFilm from '../templates/itemFilm.hbs';
 
-const createCardFunc = items => items.map(item => itemFilm(item)).join('');
+const createCardsFunc = items => items.map(item => itemFilm(item)).join('');
 
-export default createCardFunc;
+export default createCardsFunc;
