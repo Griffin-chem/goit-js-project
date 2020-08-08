afeb8c654f2a0c8ac2c76719d81c73e8fe1e32d8
IgorMazur78 <spanishman0605@gmail.com>
2020-08-08 11:02:41 +0300

    third_Header_and_Footer

--- src/dom/refs.js
+++ src/dom/refs.js
@@ -7,4 +7,16 @@ export default {
   nextBtn: document.querySelector('#next'),
   numberPage: document.querySelector('.numberPage'),
   errorDiv: document.querySelector('.errorSearch'),
+  // ===================================
+navHome:document.querySelector('.js-link-to-home'),
+navLibrary:document.querySelector('.js-link-to-myLibrary'),
+mainLibraryWatched:document.querySelector('.library-watched'),
+mainPage: document.querySelector('.main-page'),
+mainLibraryQueue:document.querySelector('.library-queue'),
+mainDetailsPage:document.querySelector('.details-page'),
+addToWatched:document.querySelector('.details-page--toWatched'),
+addToQueue:document.querySelector('.details-page--toQueue'),
+logo:document.querySelector('.js-logo-block'),
+
+
 };
--- src/header/navigation.js
+++ src/header/navigation.js
@@ -1,73 +1,118 @@
-// 'use strict';
-// // import {  } from "module";
-// const itemHome = document.querySelector('.item_home');
-// const itemMyLibrary = document.querySelector('.item_myLibrary');
+import refs from '../dom/refs';
+// import { join } from 'lodash';
+// export default {
+//     homePageGallery: document.querySelector('.js-main-page'),
+//     formInput: document.querySelector('.formSearch'),
+//     input: document.querySelector('.inputSearch'),
+//     divPagination: document.querySelector('.pagination'),
+//     prevBtn: document.querySelector('#prev'),
+//     nextBtn: document.querySelector('#next'),
+//     numberPage: document.querySelector('.numberPage'),
+//     errorDiv: document.querySelector('.errorSearch'),
+//     // ======================
+// navHome:document.querySelector('.js-link-to-home'),
+//    mainPage: document.querySelector('.main-page'),
+// navLibrary:document.querySelector('.js-link-to-myLibrary'),
+// mainLibraryWatched:document.querySelector('.js-library-watched'),
+// mainLibraryQueue:document.querySelector('.js-library-queue'),
+// mainDetailsPage:document.querySelector('.js-details-page'),
+// addToWatched:document.querySelector('.js-details-page--toWatched'),
+// addToQueue:document.querySelector('.js-details-page--toQueue'),
+// logo:document.querySelector('.logo-block--logo'),
+// };
+// ==========================
+let selectFilm;
 
-// let selectFilm; //
+// =====================================
 
-// const prevBtn = document.querySelector('#prev');
-// const nextBtn = document.querySelector('#next');
 
-// // ====================================
-// import moviesApi from './services/moviesApi';
-//  const state = {
-//      pageNumber : 1,
-//  listFilms : [],
-// }
-// const fetchPopularMoviesList = () => {
-//   /* запускаем loader */
-  
-//   moviesApi
-//     .fetchPopularMovies(url)
-//     .then(/* ваш код */)
+// ========================
+function activeHomePage() {
+ 
+//   if (event.target === refs.navHome || refs.logo) 
+    // показывает домашнюю страницу
+    if (refs.mainPage.classList.contains('is-hidden'))
+      refs.mainPage.classList.remove('is-hidden');
     
-//         //*функция прорисовки */
-
+    // прячет остальные страницы if(this.classList.contains('active'))
     
-//     .catch(
-//     /* ловим ошибку */)
-//     .finally(/* вырубаем loader */);
-// };
+    refs.mainLibraryWatched.classList.add('is-hidden');
+    refs.mainDetailsPage.classList.add('is-hidden');
+    refs.mainLibraryQueue.classList.add('is-hidden');
 
-// const fetchGenres = () => {
-//   moviesApi.fetchGenres().then(/* ваш код */).catch(/* ловим ошибку */);
-// };
-// const inputValue = 'batman';
-// const fetchFilms = () => {
-//   /* запускаем loader */
-//   moviesApi
-//     .fetchMoviesWithQuery(inputValue, pageNumber)
-//     .then(/* ваш код */)
-//     .catch(/* ловим ошибку */)
-//     .finally(/* вырубаем loader */);
-// };
+    // вешает слушателей на кнопку вперед и назад
 
-
-
-
-
-// // ==================================
-// const activeHomePage = () => {
-
-//   prevBtn.addEventListener('click',callback);
-//   nextBtn.addEventListener('click',callback);
-
-// };
-// // lightboxOpen.classList.add("is-open");
-// // prevBtn
-// // nextBtn
-// ===============
-// /function activeHomePage (){
-// показывает домашнюю страницу 
-//прячет остальные
-// вешает слушателей на кнопку вперед и назад 
-//  удаляет ненужных всех слушателей 
+    // refs.nextBtn.addEventListener('click', callback);
+    // refs.prevBtn.addEventListener('click', callbak);
+    // удаляет ненужных всех слушателей
+    // refs.addToWatched.removeEventListener('click', callback);
+    // refs.addToQueue.removeEventListener('click', callback);
+//   }
+  // (таких 4 во всем проекте не нужных на этой странице);
 }
+
 // =========================
-// function activeLibraryPage(){
-    // показывает страницу с библиотекой
-    // 
-    // 
-    // 
+function activeLibraryPage() {
+          // показывает страницу с библиотекой
+    if (refs.mainLibraryQueue.classList.contains('is-hidden'))
+      refs.mainLibraryQueue.classList.remove('is-hidden');
+    // console.log(refs.navLibrary);
+    // console.log(6789);
+    // прячет остальные
+    refs.mainPage.classList.add('is-hidden')
+    refs.mainLibraryWatched.classList.add('is-hidden');
+    refs.mainDetailsPage.classList.add('is-hidden');
+
+    // запускает функцию отрисовки фильмов из очереди drawQueueFilmList
+    // drawQueueFilmList();
+    // (которую сделает пятый участник)
+
+    // добавляет кнопке списка очереди фильмов эффект
+
+    // выбранной с помощью класса
+
+    // удаляет ненужных всех слушателей (таких 4 во всем проекте не
+    // нужных на этой странице);
+    // refs.addToWatched.removeEventListener('click', callback);
+    // refs.addToQueue.removeEventListener('click', callback);
+//   }
+}
+// ===================
+// function activeDetailsPage (movieId,itsLibraryFilm){
+//     el.preventDefault();
+    // if(el.target === img(картинка из HomePage)){
+// if(img(картинка из HomePage))
+    // }
+// movieId,itsLibraryFilm((это bool), и в зависимости
+// от того это выбранный фильм с домашней страницы или из библиотеки*/))
+
+// показывает страницу детальной отрисовки фильма
+
+// прячет остальные страницы
+//     refs.homePageGallery.classList.add('hidden');
+//     refs.mainLibraryWatched.classList.add('hidden');
+//     refs.mainLibraryQueue.classList.add('hidden');
+// // заполняет глобальную переменную selectFilm нужным объектом
+// selectFilm = ( нужный обьект) 
+// запускает функцию  showDetails( эту функцию делает №4)
+//   showDetails();
+// вешает слушателей на кнопки добавления/удаления фильмов в очередь просмотра
+  // refs.addToWatched.EventListener('click', callback);
+    // refs.addToQueue.EventListener('click', callback);
+  
+// вешает слушателей на кнопки добавления/удаления фильмов из просмотренных
+//  со страницы detailsPage
+
+// удаляет ненужных всех слушателей (таких 4 во всем проекте
+// не нужных на этой странице)
+
 // }
+
+// ==================
+// вешаем слушателей на переход на домашнюю страницу и страницу библиотеки в хедере.
+refs.navHome.addEventListener('click', activeHomePage);
+refs.navLibrary.addEventListener('click', activeLibraryPage);
+// ===================
+// на логотип повесить запуск функции activeHomePage, чтобы при клике туда возвращаться.
+refs.logo.addEventListener('click', activeHomePage);
 // ===================
