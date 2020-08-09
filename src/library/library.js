3a4d0244d529d4105d3ccf7f178a4bc80c22dd6e
LeanNSP <nsp.lean@gmail.com>
2020-08-09 20:45:33 +0300

    adds showDetails

--- src/library/library.js
+++ src/library/library.js
@@ -1,24 +1,34 @@
-import filmsQueue from'./movies';
-import filmsWached from'./movies';
+import filmsQueue from './movies';
+import filmsWached from './movies';
 import refs from '../dom/refs';
-import createLibraryCardsFunc from '../utils/createCardsFunc'
+import createLibraryCardsFunc from '../utils/createCardsFunc';
 
+refs.libraryGallery.insertAdjacentHTML(
+  'beforeend',
+  createLibraryCardsFunc(filmsQueue),
+);
 
-refs.libraryGallery.insertAdjacentHTML('beforeend', createLibraryCardsFunc(filmsQueue));
-
-refs.libraryGallery.addEventListener('click', ({target}) =>{
-activeDetailsPage(target.id, true)});
+refs.libraryGallery.addEventListener('click', ({ target }) => {
+  activeDetailsPage(target.id, true);
+});
 // потом удалить :
 const activeDetailsPage = (movieId, boole) => {
-    console.log(movieId);
-    console.log(boole);
+  console.log(movieId);
+  console.log(boole);
+};
+function createLibraryGallery(target, button, films) {
+  target.classList.add('active-but-lib');
+  button.classList.remove('active-but-lib');
+  refs.libraryGallery.insertAdjacentHTML(
+    'beforeend',
+    createLibraryCardsFunc(films),
+  );
 }
-function createLibraryGallery(target, button, films){
-    target.classList.add('active-but-lib');
-    button.classList.remove('active-but-lib');
-    refs.libraryGallery.insertAdjacentHTML('beforeend', createLibraryCardsFunc(films));
-  }
 
 //   ---
-refs.buttWatch.addEventListener('click', ({target}) => createLibraryGallery(target, refs.buttQue, filmsWached));
-refs.buttQue.addEventListener('click', ({target}) => createLibraryGallery(target, refs.buttWatch, filmsQueue));
+refs.buttWatch.addEventListener('click', ({ target }) =>
+  createLibraryGallery(target, refs.buttQue, filmsWached),
+);
+refs.buttQue.addEventListener('click', ({ target }) =>
+  createLibraryGallery(target, refs.buttWatch, filmsQueue),
+);
