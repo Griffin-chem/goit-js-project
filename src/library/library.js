9d1a04e5211ce0016650e39b183e07e4acc0f880
Griffin <chemist_khnu@rambler.ru>
2020-08-16 22:23:48 +0300

    Library Paginanion fixed

--- src/library/library.js
+++ src/library/library.js
@@ -1,7 +1,10 @@
 import refs from '../dom/refs';
 import createCardsFunc from '../utils/createCardsFunc';
+import globalValue from '../globalValue/globalValue';
 
 const createGallery = (films, galleryLibName) => {
+  total = 6;
+  pageNow = 1;
   refs.errorLib.classList.add('displayNone');
   const filmsix = films.filter((film, index) => {
     if (index <= 5) {
@@ -9,13 +12,14 @@ const createGallery = (films, galleryLibName) => {
     }
   });
 
-  films.length <= 6
+  if (films.length > 0) {
+    refs.divPaginationLib.classList.remove('displayNone');
+    refs.prevBtnLib.classList.add('hidden');
+    refs.nextBtnLib.classList.remove('hidden');
+    refs.numberPageLib.textContent = `1 - ${Math.ceil(films.length / 6)}`;
+    films.length <= 6
     ? refs.divPaginationLib.classList.add('displayNone')
     : refs.divPaginationLib.classList.remove('displayNone');
-
-  if (films.length > 0) {
-    refs.divPaginationLib.classList.remove('displayNone')
-    refs.numberPageLib.textContent = `1 - ${Math.ceil(films.length / 6)}`;
     refs.libraryGallery.insertAdjacentHTML(
       'beforeend',
       createCardsFunc(filmsix),
@@ -36,3 +40,79 @@ const createLibraryGallery = (target, button, films) => {
 };
 
 export { createGallery, createLibraryGallery };
+
+refs.prevBtnLib.classList.add('hidden');
+
+let pageNow;
+let total = 6;
+let filmsNumbers;
+let arrFilms;
+
+const incrementPage = () => {
+  total += 6;
+};
+const decrementPage = () => {
+  if (total >= 6) {
+    total -= 6;
+  } else if (total === 0) {
+    return;
+  }
+};
+
+
+refs.divPaginationLib.addEventListener('click', paginationLibrary);
+
+function paginationLibrary(e) {
+  e.preventDefault();
+
+  if (refs.buttWatch.classList.contains('active-but-lib')) {
+    filmsNumbers = globalValue.getFilmsWatched().length;
+    arrFilms = globalValue.getFilmsWatched();
+  } else if (refs.buttQue.classList.contains('active-but-lib')) {
+    arrFilms = globalValue.getFilmsQueue();
+    filmsNumbers = globalValue.getFilmsQueue().length;
+  } else {
+    return;
+  }
+
+  const allFilmsPages = Math.ceil(filmsNumbers / 6);
+
+  if (e.target.id === 'prevLib' && total > 6) {
+    refs.libraryGallery.innerHTML = '';
+    const prevPageFilms = arrFilms.filter((film, index) => {
+      if (index < total - 6 && index >= total - 12) {
+        return film;
+      }
+    });
+    refs.libraryGallery.insertAdjacentHTML(
+      'beforeend',
+      createCardsFunc(prevPageFilms),
+    );
+    decrementPage();
+  } else if (e.target.id === 'nextLib' && total <= filmsNumbers) {
+    refs.libraryGallery.innerHTML = '';
+    const nextPageFilms = arrFilms.filter((film, index) => {
+      if (index > total - 1 && index < total + 6) {
+        return film;
+      }
+    });
+    refs.libraryGallery.insertAdjacentHTML(
+      'beforeend',
+      createCardsFunc(nextPageFilms),
+    );
+    incrementPage();
+  } else {
+    return;
+  }
+  pageNow = total / 6;
+
+  pageNow > 1
+    ? refs.prevBtnLib.classList.remove('hidden')
+    : refs.prevBtnLib.classList.add('hidden');
+
+  pageNow === allFilmsPages
+    ? refs.nextBtnLib.classList.add('hidden')
+    : refs.nextBtnLib.classList.remove('hidden');
+
+  refs.numberPageLib.textContent = `${pageNow} - ${allFilmsPages}`;
+}
